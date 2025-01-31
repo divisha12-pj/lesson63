eaaba59b76d293b357795b5b4dadb52363e5e791
divishajain12-pj <divishajain1210@gmail.com>
2025-01-31 21:43:10 +0530

    act2

---
+++ act2.py
@@ -0,0 +1,16 @@
+#Write a Program to find the element not making a pair.
+
+def oddoccuringnum(lst):
+    res=0
+    for element in lst :
+        return res
+
+arr=[]
+
+n=int(input("enter the length of list"))
+while(n):
+    num=int(input("enter a number"))
+    arr.append(num)
+    n-=1
+
+print("odd occuring no.is ",oddoccuringnum(arr))
